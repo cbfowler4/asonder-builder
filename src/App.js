36dfa153dd0476057c502958ee596e2047b41870
Bryan <cbfowler4@gmail.com>
2019-09-12 20:48:41 -0400

    adds our own model!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="header">
-            <div className="intro">
-              Who dat
-            </div>
-            <div className="reveal">
-              UN // CUT
-            </div>
+const App = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="header">
+          <div className="intro">
+            Who dat
           </div>
-        </header>
-      </div>
-    );
-  }
+          <div className="reveal">
+            UN // CUT
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
